0c0b4dfa7c1e5c4919537c5f738aae2c88ac8752
DCKAP <varshithaksdckap@gmail.com>
2023-10-13 16:20:17 +0530

    changecolor

---
+++ index.js
@@ -0,0 +1,56 @@
+// let colo1 = document.getElementById("btn1");
+// let colo2 = document.getElementById("btn2");
+// let colo3 = document.getElementById("btn3");
+// colo1.addEventListener("click", coLour);
+// function coLour()
+// {
+//     colo1.style.backgroundColor = "Indigo"
+//     colo1.style.color = "Yellow"
+//     colo1.innerHTML = "Clicked";
+// }
+// colo2.addEventListener("click", cOlour);
+// function cOlour()
+// {
+//     colo2.style.backgroundColor = "Indigo"
+//     colo2.style.color = "Yellow"
+//     colo2.innerHTML = "Clicked";
+// }
+// colo3.addEventListener("click", colOur);
+// function colOur()
+// {
+//     colo3.style.backgroundColor = "Indigo"
+//     colo3.style.color = "Yellow"
+//     colo3.innerHTML = "Clicked";
+// }
+
+
+//------------------------------------Another method using flag-----------------------------------------------------------
+// let btn = document.querySelectorAll("button");
+
+// console.log(btn);
+
+// for(let i=0; i<= btn.length-1;i++)
+// {
+//     console.log("loop")
+//     btn[i].flag = false;
+//     btn[i].addEventListener("click",function()
+//     {
+//         console.log("Incoming");
+//         console.log("btn flag check : "+btn[i].flag )
+//         if(btn[i].flag == false)
+//         {
+//             console.log("if block")
+//             btn[i].style.backgroundColor = "green";
+//             btn[i].innerText ="Clicked";
+//             btn[i].flag = true;
+//             console.log(btn[i])
+//         }
+//         else
+//         {
+//             btn[i].style.backgroundColor = "#9551d4";
+//             btn[i].innerText ="click me";
+
+//             btn[i].flag = false;
+//         }
+//     });
+// }
